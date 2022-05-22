7e924f97f5276e86eb25e809bc71a97a48ee427b
leminh4000 <leminh2000@gmail.com>
2022-05-22 16:30:59 +0700

    test

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -3,15 +3,17 @@ import {SalesPerson} from "./sales-person";
 
 @Component({
   selector: 'app-sales-person-list',
-  templateUrl: './sales-person-list.component.html',
+  templateUrl: './sales-person-list-bootstrap.component.html',
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
 
   //create an array of objects
   salesPersonList: SalesPerson[] = [
-    new SalesPerson("Le", "Minh", "leminh@gmail.com", 1),
-    new SalesPerson("Do", "Tuan", "dotuan@gmail.com", 2),
+    new SalesPerson("Le", "Minh", "leminh@gmail.com", 10000),
+    new SalesPerson("Do", "Tuan", "dotuan@gmail.com", 20000),
+    new SalesPerson("Do", "Tuan", "dotuan@gmail.com", 30000),
+    new SalesPerson("Do", "Tuan", "dotuan@gmail.com", 40000),
   ];
 
 
